422249d733bf1c20839ae2acc18719ee2811b43e
Eslam Elfateh <eslam.elfateh.94@gmail.com>
2020-05-05 01:10:25 +0200

    frist step in home page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component'
+
+import './homepage.styles.scss';
+
+
+const HomePgae = () => (
+	<div className='homePage'>
+		<Directory/>
+	</div>
+
+	)
+
+
+	export default HomePgae;
