a1c366dfa3df1cb3cdfbe5bd62deb21370ed05cb
Eslam Elfateh <eslam.elfateh.94@gmail.com>
2020-05-20 05:10:56 +0200

    integrated firebase into app , creat sign in page with sign in component and sign in with google

---
+++ src/components/Sign-in/sign-in.component.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import './sign-in.styles.scss';
+
+import {signInWithGoogle} from '../../firebase/firebase.utils';
+
+class SignIn extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email: '' ,
+      password: ''
+    }
+  }
+
+  handleSubmite = (e) => {
+    e.preventDefault() ;
+
+    this.setState({
+      email: '',
+      password: ''
+    })
+  }
+
+  handleChange = (e) => {
+    const {value , name} = e.target ;
+    this.setState( {[name] : value})
+  }
+
+  render() {
+    const {email , password} = this.state
+
+    return (
+      <div className='sign-in'>
+        <h2>I already have an acount</h2>
+        <span>Sign in with your email and password</span>
+        
+        <form  onSubmit={this.handleSubmite}>
+          <FormInput 
+              type="email" 
+              name='email'
+              handleChange={this.handleChange} 
+              label='email'
+              value={email} required/>
+
+          <FormInput 
+              type="password" 
+              name='password' 
+              handleChange={this.handleChange}
+              label='password'
+              value={password} required/>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn > Sign in  with Google</CustomButton>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+export default SignIn ;
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+
+import SignIn from '../../components/Sign-in/sign-in.component'
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div>
+      <SignIn/>
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
