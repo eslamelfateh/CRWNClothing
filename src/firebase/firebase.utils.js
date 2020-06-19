5201b1cd4659d0bae8741e8663470dfb5f7be5cd
Eslam Elfateh <eslam.elfateh.94@gmail.com>
2020-06-19 21:47:36 +0200

    implement firebase utility and SignUp component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,9 +15,37 @@ const firebaseConfig = {
   measurementId: "G-6R0EMPDF4Y"
 };
 
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  // console.log(firestore.doc('users/1243324235dfgsf'))
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
